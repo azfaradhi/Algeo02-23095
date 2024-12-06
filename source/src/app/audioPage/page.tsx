d0e58ef7ba33723084d0db27b1537e28300fd802
Barru Adi <barru.adi@gmail.com>
2024-12-06 17:18:49 +0700

    add: folder structure

---
+++ source/src/app/audioPage/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { useState } from "react";
+
+export default function AudioPage() {
+  const [audioFile, setAudioFile] = useState('');
+
+  return (
+    <div>
+      <h1>Audio Page</h1>
+    </div>
+  );
+}
--- source/src/app/page.tsx
+++ source/src/app/page.tsx
@@ -1,8 +1,12 @@
+'use client';
+
 import { useEffect, useState } from 'react';
 import { fetchGreeting } from '../services/api';
+import { useRouter } from 'next/navigation';
 
 export default function HomePage() {
   const [greeting, setGreeting] = useState('');
+  const router = useRouter();
 
   useEffect(() => {
     const getGreeting = async () => {
@@ -12,9 +16,15 @@ export default function HomePage() {
     getGreeting();
   }, []);
 
+  const handleRedirect = (path: string) => {
+    router.push(path);
+  }
+
   return (
     <div>
       <p>Greeting: {greeting}</p>
+      <button onClick={() => handleRedirect('/audioPage')}>Go to Audio Page</button>
+      <button onClick={() => handleRedirect('/imagePage')}>Go to Image Page</button>
     </div>
   );
 }
