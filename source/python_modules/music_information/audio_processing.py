9ef0364b253bf9eee80c70cee2eddd65036131d6
azfaradhi <aradhihakim@gmail.com>
2024-12-15 20:04:52 +0700

    fix some bug when processing database

--- source/python_modules/music_information/audio_processing.py
+++ source/python_modules/music_information/audio_processing.py
@@ -12,14 +12,14 @@ def wav_processing(namafile):
 
     midiNotes = []
 
-    while len(midiNotes) < 20:
+    while len(midiNotes) < 30:
         for t in range(pitches.shape[1]):
             index = magnitudes[:, t].argmax()
             pitch = pitches[index, t]
             if pitch > 0: 
                 midiNotes.append(librosa.hz_to_midi(pitch))
 
-        if len(midiNotes) >= 20:
+        if len(midiNotes) >= 30:
             break
 
     # normalisasi
@@ -29,39 +29,48 @@ def wav_processing(namafile):
 
     # pembuatan windows
     windows = []
-    for start in range(0, len(normalTempo) - 20 + 1, 4):
-        window = normalTempo[start:start + 20]
+    for start in range(0, len(normalTempo) - 30 + 1,6):
+        window = normalTempo[start:start + 30]
         windows.append(window)
 
     return windows
 
 def midi_processing(namafile):
-    midi = mido.MidiFile(namafile)
-    midiNotes = []
-    channel = 0
+    try:
+        midi = mido.MidiFile(namafile)
+        midiNotes = []
+        channel = 0
 
-    while (True):
-        for track in midi.tracks:
-            for msg in track:
-                if msg.type == 'note_on' and msg.channel == channel and msg.velocity > 0:
-                    midiNotes.append(msg.note)
-        if len(midiNotes) > 20:
-            break
+        while (True):
+            for track in midi.tracks:
+                for msg in track:
+                    if msg.type == 'note_on' and msg.channel == channel and msg.velocity > 0:
+                        if (0<= msg.note <= 127):
+                            midiNotes.append(msg.note)
+            if len(midiNotes) > 30:
+                break
+            else:
+                channel += 1
+
+        # normalisasi
+        meanPitch = np.mean(midiNotes)
+        stdPitch = np.std(midiNotes)
+        if stdPitch == 0:
+            normalTempo = [0 for _ in midiNotes]  # Set all normalized values to 0
         else:
-            channel += 1
+            normalTempo = [(note - meanPitch) / stdPitch for note in midiNotes]
+        
+        normalTempo = np.clip(normalTempo, 0, 127)
 
+        # pembuatan windows
+        windows = []
+        for start in range(0, len(normalTempo) -30 + 1, 6):
+            window = normalTempo[start:start + 30]
+            windows.append(window)
+        return windows
+    except (OSError, ValueError) as e:
+        return  []# Return an empty list or handle it as needed
 
-    # normalisasi
-    meanPitch = np.mean(midiNotes)
-    stdPitch = np.std(midiNotes)
-    normalTempo = [(note - meanPitch) / stdPitch for note in midiNotes]
-
-    # pembuatan windows
-    windows = []
-    for start in range(0, len(normalTempo) -20 + 1, 4):
-        window = normalTempo[start:start + 20]
-        windows.append(window)
-    return windows
 
 
 
