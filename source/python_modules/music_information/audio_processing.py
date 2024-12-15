01f7398e6d3f23c9e10db11659ed9801abbad657
azfaradhi <aradhihakim@gmail.com>
2024-12-15 10:43:28 +0700

    adding wav processing

--- source/python_modules/music_information/audio_processing.py
+++ source/python_modules/music_information/audio_processing.py
@@ -1,6 +1,39 @@
 import mido
 import numpy as np
+import librosa
 
+def wav_processing(namafile):
+    
+    #loading
+    y, sr = librosa.load(namafile)
+
+    # extract pitch
+    pitches, magnitudes = librosa.piptrack(y=y, sr=sr)
+
+    midiNotes = []
+
+    while len(midiNotes) < 20:
+        for t in range(pitches.shape[1]):
+            index = magnitudes[:, t].argmax()
+            pitch = pitches[index, t]
+            if pitch > 0: 
+                midiNotes.append(librosa.hz_to_midi(pitch))
+
+        if len(midiNotes) >= 20:
+            break
+
+    # normalisasi
+    meanPitch = np.mean(midiNotes)
+    stdPitch = np.std(midiNotes)
+    normalTempo = [(note - meanPitch) / stdPitch for note in midiNotes]
+
+    # pembuatan windows
+    windows = []
+    for start in range(0, len(normalTempo) - 20 + 1, 4):
+        window = normalTempo[start:start + 20]
+        windows.append(window)
+
+    return windows
 
 def midi_processing(namafile):
     midi = mido.MidiFile(namafile)
@@ -16,21 +49,27 @@ def midi_processing(namafile):
             break
         else:
             channel += 1
-    # print(channel)
-    # print(midiNotes)
 
-    
+
+    # normalisasi
     meanPitch = np.mean(midiNotes)
     stdPitch = np.std(midiNotes)
     normalTempo = [(note - meanPitch) / stdPitch for note in midiNotes]
 
+    # pembuatan windows
     windows = []
     for start in range(0, len(normalTempo) -20 + 1, 4):
         window = normalTempo[start:start + 20]
         windows.append(window)
     return windows
 
+
+
 # namafile = "x (26).mid"
 # midi = mido.MidiFile(namafile)
 # a = midi_processing(namafile)
 # print(a)
+
+# namafile = "beethoven.wav"
+# windows = wav_processing(namafile)
+# print(windows)
