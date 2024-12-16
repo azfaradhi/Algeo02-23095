ffb4b76ecd1a75b4c240ee990b35a513f267aa34
Barru Adi <barru.adi@gmail.com>
2024-12-16 18:57:58 +0700

    fix: if no album

--- source/src/components/audio/audioSubmit.tsx
+++ source/src/components/audio/audioSubmit.tsx
@@ -109,6 +109,12 @@ const AudioSubmit = () => {
                                         <AudioPlayCard album={matchAlbum} score={item.score} />
                                     </div>
                                 )}
+                                {!matchAlbum && (
+                                    <div className="mt-4 flex w-full justify-between">
+                                      <h2 className="text-xl">{item.namafile}</h2>
+                                      <h2 className="text-xl">{(item.score * 100 ).toFixed(2)}%</h2>
+                                    </div>
+                                )}
                             </li>
                         );
                     })}
